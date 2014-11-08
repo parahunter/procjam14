83593e7cd0facda617eedbdef569923c60da3d17
parahunter <parahunter@gmail.com>
2014-11-08 17:58:40 +0000

    work on basic prefabs

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -3,12 +3,16 @@ using System.Collections;
 
 public class CameraControl : MonoBehaviour 
 {
-	
-	
+	public float horizontalSpeed = 5f;
+	public float verticalSpeed = 5f;
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		
+		float mouseX = Input.GetAxis("Mouse X");
+		float mouseY = Input.GetAxis("Mouse Y");
+			
+		transform.Rotate(new Vector3(0, mouseX * horizontalSpeed * Time.deltaTime, 0), Space.World);
+		transform.Rotate(new Vector3(mouseY * verticalSpeed * Time.deltaTime, 0, 0 ), Space.Self); 
 	}
 }
---
+++ Assets/Scripts/Goal.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Goal : MonoBehaviour 
+{
+	public event System.Action onPlayerReachedGoal;
+	
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.CompareTag("Player"))
+		{
+			if(onPlayerReachedGoal != null)
+				onPlayerReachedGoal();
+		}
+	}
+	
+}
