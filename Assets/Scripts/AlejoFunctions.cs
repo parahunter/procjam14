0fe9450190363d296482974768f83977ca5098b1
alejocamaleon <alejo.acevedo@civantos.com>
2014-11-09 00:16:31 +0000

    Surface point function
    given a gameObject and a point in its local space the function returns
    the closest point in the surface of it's collider

---
+++ Assets/Scripts/AlejoFunctions.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AlejoFunctions : MonoBehaviour {
+
+	public Transform handler;
+	public Transform testSubject;
+	public Transform child;
+
+	// Use this for initialization
+	void Start()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+		child.position = SurfacePoint(testSubject.gameObject, handler.localPosition);
+	
+	}
+
+	Vector3 SurfacePoint(GameObject root, Vector3 anchor)
+	{
+		Collider thisCollider = root.GetComponent<Collider>();
+		if(thisCollider != null)
+		{
+			anchor = root.transform.TransformPoint(anchor);
+			Vector3 dir = anchor - root.transform.position;
+			float rayMagnitude = 2 * root.transform.lossyScale.magnitude * (dir.magnitude + 2);
+			//Vector3 origin = (anchor - dir) * rayMagnitude;
+			Vector3 origin = anchor + dir * rayMagnitude;
+
+			RaycastHit hitInfo;
+
+			Debug.DrawRay(origin, -dir, Color.blue);
+			if(thisCollider.Raycast(new Ray(origin, -dir), out hitInfo, 200))
+			{
+				return hitInfo.point;
+			}
+		}
+		return Vector3.zero;
+	}
+
+}
